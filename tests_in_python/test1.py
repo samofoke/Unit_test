0d448d46ff99adc7869f9a17b18ea82a6a421c60
Sabata <samofoke@student.wethinkcode.co.za>
2020-09-02 12:13:51 +0200

    second solution works

--- tests_in_python/test1.py
+++ tests_in_python/test1.py
@@ -4,21 +4,27 @@ from test2 import solve_me
 
 
 
-class Test1(unittest.TestCase):
+class Test_First(unittest.TestCase):
 
     def test_basics_1(self):
         r = [4, 12, 3, 20, 50, 8, 5, 13, 30];
-        x = 8;
+        x = 12;
         n = len(r);
         res = solve_me(r, n, x)
         if (res == -1):
             self.assertEqual(res, -1, print ('it returns -1 as expected.'))
-        else:
+        elif (res):
             self.assertEqual(res, res, print('it returns the value of x as expected',res))
+        else:
+            self.fail("Please try again your solution is not working.")
 
-    # def test_basics_2(self):
-    #     pass
-
+    def test_basics_2(self):
+        r = [4, 12, 3, 20, 50, 8, 5, 13, 30];
+        x = None;
+        n = len(r);
+        res = solve_me(r, n, x)
+        if (res == None):
+            self.assertEqual(res, None, print('Nothing is returned.'))
     # def test_basics_3(self):
     #     pass
 
--- tests_in_python/test2.py
+++ tests_in_python/test2.py
@@ -5,7 +5,17 @@
 
 def solve_me(arr, n, x):
     
-    for i in range (0, n):
+    # for i in range (0, n):
+    #     if (arr[i] == x):
+    #         return i
+    #     elif (arr[i] == 0):
+    #         return i
+    # return -1
+    
+    #second solution also works.
+    i = 0
+    while (i in range(0,n)):
         if (arr[i] == x):
             return i
-    return -1
+            i = i + 1
+        return -1
